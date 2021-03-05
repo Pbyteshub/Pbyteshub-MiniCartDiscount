08c71ce3cde496fab2fd208802b21887d5107d06
Pbytes Hub <79686261+Pbyteshub@users.noreply.github.com>
2021-03-05 23:58:16 +0530

    Up Latest Pbyteshub_MiniCartDiscount Extension

---
+++ MiniCartDiscount/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Pbyteshub All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Pbyteshub_MiniCartDiscount', __DIR__);
+
